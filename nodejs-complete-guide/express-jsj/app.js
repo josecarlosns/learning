29374373a1a1e2502bbdadbc56392f01ee5fc29a
Jose Silva <jcnascimento1994@gmail.com>
2026-03-22 20:47:46 -0300

    nc1p2 added express middleware

--- nodejs-complete-guide/express-jsj/app.js
+++ nodejs-complete-guide/express-jsj/app.js
@@ -1,5 +1,8 @@
 const http = require('http');
+const express = require('express');
 
-const server = http.createServer();
+const app = express();
+
+const server = http.createServer(app);
 
 server.listen(3000);
