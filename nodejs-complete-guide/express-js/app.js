b65f316275330af7d026d596d0f63b6524d3d2cd
Jose Silva <jcnascimento1994@gmail.com>
2026-03-23 12:14:55 -0300

    nc1p2 adding static css files

--- nodejs-complete-guide/express-js/app.js
+++ nodejs-complete-guide/express-js/app.js
@@ -4,9 +4,11 @@ import express from "express";
 import { adminRoutes } from "./routes/admin.js";
 import { errorRoute } from "./routes/error.js";
 import { shopRoutes } from "./routes/shop.js";
+import { getPath } from "./utils/paths.js";
 
 const app = express();
 app.use(bodyParser.urlencoded());
+app.use(express.static(getPath("public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
