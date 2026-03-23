34df62cfd1db827ad9cb27a2dbecfe1e12e65741
Jose Silva <jcnascimento1994@gmail.com>
2026-03-22 21:02:51 -0300

    nc1p2 added json response to request, debug config with nodemon

--- nodejs-complete-guide/express-jsj/app.js
+++ nodejs-complete-guide/express-jsj/app.js
@@ -3,6 +3,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('in the minddelware without the next function');
+
+  res.json({
+    number: 123.5,
+    boolean: true,
+    msg: 'Hello world!'
+  });
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
