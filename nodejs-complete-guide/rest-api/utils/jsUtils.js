6f0e180de04b6c56155df19cade2048c81f52eb0
Jose Silva <jcnascimento1994@gmail.com>
2026-04-02 14:38:08 -0300

    nc1p4 added new functions to js utils

--- nodejs-complete-guide/rest-api/utils/jsUtils.js
+++ nodejs-complete-guide/rest-api/utils/jsUtils.js
@@ -6,8 +6,18 @@ function getPath(...paths) {
   return path.join(rootDir, ...paths);
 }
 
-function isEmptyObject(object) {
-  return !object || Object.keys(object) === 0;
+function isNullOrUndefined(value) {
+  return value === null || value === undefined;
 }
 
-export { getPath, isEmptyObject };
+function isEmptyObject(object) {
+  return isNullOrUndefined(object) || Object.keys(object) === 0;
+}
+
+function isEmptyString(string) {
+  return (
+    !isNullOrUndefined(string) && typeof string === "string" && string === ""
+  );
+}
+
+export { getPath, isEmptyObject, isEmptyString, isNullOrUndefined };
